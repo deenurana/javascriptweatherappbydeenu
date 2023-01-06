9f9042d98ecc94005f3283df0cc2205cec160bbb
deenurana <115528947+deenurana@users.noreply.github.com>
2023-01-06 18:57:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let featchURL = "https://api.openweathermap.org/data/2.5/weather?q="
+let cityName = '';
+let apiKey = "05017894b8b0ac83af72659f3dc9d03c"
+
+
+document.querySelector('.search_box').addEventListener('keyup', event => {
+    let { keyCode, target: input } = event
+
+    if (keyCode === 13) {
+        console.log(input.value)
+        cityName = input.value
+        weatherData(cityName)
+
+        document.querySelector('.search_box').value = ''
+    }
+
+})
+function weatherData(CityName){
+    console.log('weather data function')
+    fetch(`${featchURL}${CityName}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(cityData =>{
+        console.log(cityData)
+        document.querySelector('.city').innerHTML = `${cityData.name}, ${cityData.sys.country}`
+        document.querySelector('.temperature').innerHTML = `${Math.floor(cityData.main.temp - 273.15)}°c`
+        document.querySelector('.weather').innerHTML = `${cityData.weather[0].main}`
+        document.querySelector('.min-max').innerHTML = `Min ${Math.floor(cityData.main.temp_min - 273.15)}°c / Max ${Math.floor(cityData.main.temp_max - 273.15)}°c`
+        document.querySelector('.wind').innerHTML = `${cityData.wind.speed}`
+    })  
+    .catch(err => alert('City not found'))      
+}
+
+weatherData('Charkhi Dadri')
